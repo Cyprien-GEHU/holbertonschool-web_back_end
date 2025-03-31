174ab18d6c1c437f6dd909174688cc94be8ee768
Cyprien <cypriengehu@example.com>
2025-03-31 13:35:56 +0200

    task 6

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,14 @@
 export default function handleResponseFromAPI(promise) {
-  return promise 
+  return promise
     .then(() => {
-        return {
-          status: 200,
-          body: 'Success',
-        };
+      console.log('Got a response from the API');
+      return {
+        status: 200,
+        body: 'Success',
+      };
     })
     .catch(() => new Error())
     .finally(() => {
-        console.log("Got a response from the API")
-    })
+      console.log('Got a response from the API');
+    });
 }

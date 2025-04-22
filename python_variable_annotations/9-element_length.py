f4cd31005f988d27bb5860e2650740f3f2aba87d
Cyprien <cypriengehu@example.com>
2025-04-22 11:29:18 +0200

    task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""
+the objective are to take the funtion element_length and :
+- annontate the below function parameters
+- return value with appropirate type
+"""
+from typing import Iterable, Tuple, List, Sequence
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """the function"""
+    return [(i, len(i)) for i in lst]

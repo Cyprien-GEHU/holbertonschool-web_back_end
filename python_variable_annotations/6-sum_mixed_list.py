c312828011dac32c061f3cbe49c541bfb99b3465
Cyprien <cypriengehu@example.com>
2025-04-22 10:55:14 +0200

    correction of the union

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -6,6 +6,6 @@ and return the result into float
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """the function sum all mix element of the list"""
     return sum(mxd_lst)

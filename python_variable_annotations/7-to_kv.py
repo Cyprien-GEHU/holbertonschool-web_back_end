5b22f1f0412e2562224c0f5ffee1f79c1279889f
Cyprien <cypriengehu@example.com>
2025-04-22 11:04:06 +0200

    task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+"""
+the function will take the string k and the int/float v
+and return the result into tuple with:
+- the first element k (string)
+- the square of the int or float v (into float)
+"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """the function sum all mix element of the list"""
+    return (k, float(v ** 2))

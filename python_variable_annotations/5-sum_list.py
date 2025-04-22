7ed58b0e7ee7f5937d5d7533ff1c3d80f6b706b9
Cyprien <cypriengehu@example.com>
2025-04-22 10:47:50 +0200

    correction with from and not type

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -3,9 +3,9 @@
 the function will sum all float of the list
 and return the result into float
 """
-type list_float = list[float]
+from typing import List
 
 
-def sum_list(input_list: list_float) -> float:
+def sum_list(input_list: List[float]) -> float:
     """the function sum all element of the list"""
     return sum(input_list)

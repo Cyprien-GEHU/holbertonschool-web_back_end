310f84dd560d6977f903d827ca148bdb49fdaed4
Cyprien <cypriengehu@example.com>
2025-04-28 11:36:22 +0200

    correction

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,6 +4,6 @@ async_generator = __import__('0-async_generator').async_generator
 from typing import List
 
 
-async def async_generator() -> List[float]:
+async def async_comprehension() -> List[float]:
     """the function"""
     return [i async for i in async_generator()]

a6e91e3875f7f2216439a2a8c05493a678609379
Cyprien <cypriengehu@example.com>
2025-04-28 11:49:45 +0200

    pycodestyle task 1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -4,7 +4,6 @@ from typing import List
 async_generator = __import__('0-async_generator').async_generator
 
 
-
 async def async_comprehension() -> List[float]:
     """the function"""
     return [i async for i in async_generator()]

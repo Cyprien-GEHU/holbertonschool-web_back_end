866cc63b4a79ca992154d52a544e045f24073803
Cyprien <cypriengehu@example.com>
2025-05-19 13:04:27 +0200

    work: creat a complex http server with a database

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+// this function will a small http server with the module Node HTTP
+
+const httpServer = require('http');
+const countStudents = require('./3-read_file_async');
+
+const host = '127.0.0.1';
+const port = 1245;
+const database = process.argv[2];
+
+const app = httpServer.createServer(async (req, res) => {
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const { studentData, studentCS, studentSWE } = await countStudents(database);
+      res.write(`Number of students: ${studentData.length}\n`);
+      res.write(`Number of students in CS: ${studentCS.length}. List: ${studentCS.join(', ')}\n`);
+      res.write(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`);
+      res.end();
+    } catch (err) {
+      res.end(err.message);
+    }
+  } else {
+    res.statusCode = 404;
+    res.end('Invalid request');
+  }
+});
+
+app.listen(port, host);
+module.exports = app;

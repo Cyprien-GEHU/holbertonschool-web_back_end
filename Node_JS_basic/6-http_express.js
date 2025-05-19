d7b20d0e31ca11602d8267b5e0db2216f13e4680
Cyprien <cypriengehu@example.com>
2025-05-19 13:28:05 +0200

    work: create a http server with express module

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+// Install a smal http server with module express
+
+const express = require('express');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(port);
+
+module.exports = app;

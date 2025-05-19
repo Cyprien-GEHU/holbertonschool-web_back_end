2455eee6785908f3907e3586688269c5756a3770
Cyprien <cypriengehu@example.com>
2025-05-19 11:35:55 +0200

    work: we read the database.csv but with async method

---
+++ Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,35 @@
+// the function will read database.csv but it's async this time !
+
+const fs = require('fs');
+
+const countStudents = async (filecsv) => {
+  let csv = '';
+
+  try {
+    csv = await fs.readFileSync(filecsv, 'utf-8');
+  } catch (err) {
+    throw new Error('cannot load the database');
+  }
+
+  const datacsv = csv.split('\n').slice(1);
+  const data = datacsv.map((line) => line.split(','))
+    .filter((student) => student.length === 4);
+
+  const studentData = data.map((student) => ({
+    firstname: student[0],
+    lastname: student[1],
+    age: student[2],
+    field: student[3],
+  }));
+
+  const studentCS = studentData.filter((data) => data.field === 'CS')
+    .map((map) => map.firstname);
+  const studentSWE = studentData.filter((data) => data.field === 'SWE')
+    .map((map) => map.firstname);
+
+  console.log(`Number of students: ${studentData.length}`);
+  console.log(`Number of students in CS: ${studentCS.length}. List: ${studentCS.join(', ')}`);
+  console.log(`Number of students in SWE: ${studentSWE.length}. List: ${studentSWE.join(', ')}`);
+};
+
+module.exports = countStudents;

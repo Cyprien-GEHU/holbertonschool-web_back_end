a69d2f957b99d0ec12818b65c17efd70ed1b659d
Cyprien <cypriengehu@example.com>
2025-05-19 10:22:05 +0200

    work: we creat a using process

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,15 @@
+// the function wiil demande th name of the user
+
+const stand = require('process')
+
+stand.stdout.write("Welcome to Holberton School, what is your name?\n")
+stand.stdin.on('readable', () => {
+    const user = stand.stdin.read();
+    if(user) {
+        stand.stdout.write(`Your name is: ${user}`);
+    }
+});
+
+stand.stdin.on('end', () => {
+    console.log('This important software is now closing')
+})

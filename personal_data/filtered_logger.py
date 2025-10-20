7b78224596005911ad236ca8d669b5bbee30acc3
Cyprien <cypriengehu@example.com>
2025-10-20 11:00:39 +0200

    fix: add annotation on the function

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,9 +4,11 @@ the programme return the log message
 """
 
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """the function to return the log message"""
     for field in fields:
         pat = rf"{field}=.*?(?={separator})|$"

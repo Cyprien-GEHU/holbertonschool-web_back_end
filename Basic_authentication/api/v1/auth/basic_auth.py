e76c38e1247536d6c00b989043881b5f8672ee17
Cyprien <cypriengehu@example.com>
2025-10-22 13:30:38 +0200

    feat: task 8, base64 decode

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,8 +9,8 @@ from api.v1.auth.auth import Auth
 
 class BasicAuth(Auth):
     """Basic authentification with Auth"""
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """basic auth base64 authorization"""
         if authorization_header is None:
             return None
@@ -20,3 +20,17 @@ class BasicAuth(Auth):
             return None
         else:
             return "".join(authorization_header.split(" ")[1:])
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """decode the base64"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            encode = base64_authorization_header.encode('utf-8')
+            base = base64.b64decode(encode)
+            return base.decode('utf-8')
+        except Exception:
+            return None

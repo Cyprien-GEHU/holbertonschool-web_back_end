a525c802ba6b508a3776a04aa06830d1594d66da
Cyprien <cypriengehu@example.com>
2025-10-22 10:09:24 +0200

    feat: task3, auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+authentication system
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth() :
+    """class authentication"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """required authentication function"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """authorization header function"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """the current user function"""
+        return None

94e40eb1de9ac3d3166b7541b71099296ce2ae66
Cyprien <cypriengehu@example.com>
2025-10-22 11:20:40 +0200

    feat: task 7, basic - base64

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,8 +3,20 @@
 Basic authentification system
 """
 
+import base64
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """Basic authentification with Auth"""
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """basic auth base64 authorization"""
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        else:
+            return "".join(authorization_header.split(" ")[1:])

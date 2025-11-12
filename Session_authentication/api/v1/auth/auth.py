99990405ddd6670822eb4579530a6efcd29f76b4
Cyprien <cypriengehu@example.com>
2025-11-12 11:35:46 +0100

    feat: task 4, get session cookie from your request

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 authentication system
 """
 from flask import request
+import os
 from typing import List, TypeVar
 
 
@@ -30,3 +31,11 @@ class Auth():
     def current_user(self, request=None) -> TypeVar('User'):
         """the current user function"""
         return None
+
+    def session_cookie(self, request=None):
+        """ get session cookie from the request"""
+        if request is None:
+            return None
+
+        session_name = os.getenv('SESSION_NAME')
+        return request.cookies.get(session_name)

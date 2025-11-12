542234d02669ee45575c7e164bc69a51f0121548
Cyprien <cypriengehu@example.com>
2025-11-12 10:50:14 +0100

    fix: document the function create_session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -13,6 +13,7 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
+        """ create a session with auth"""
         if user_id is None or not isinstance(user_id, str):
             return None
         session_id = str(uuid.uuid4())

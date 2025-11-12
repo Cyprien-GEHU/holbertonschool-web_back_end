1e053bcb2d0e0b1d8d28418014f55e9618253f51
Cyprien <cypriengehu@example.com>
2025-11-12 10:41:35 +0100

    feat: task 2 create a session (not empty this time)

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,7 +2,7 @@
 """
 session authentication system
 """
-import base64
+import uuid
 from api.v1.auth.auth import Auth
 from typing import TypeVar
 from models.user import User
@@ -10,3 +10,11 @@ from models.user import User
 
 class SessionAuth(Auth):
     """class sessionAuth"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id

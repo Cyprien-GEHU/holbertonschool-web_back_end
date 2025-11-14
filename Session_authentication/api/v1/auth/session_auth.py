c940c9e310d44cbe93bdc6bc4309466c3aecbdd0
Cyprien <cypriengehu@example.com>
2025-11-14 10:01:54 +0100

    feat: task 6, find a user with a session id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -25,3 +25,8 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ identify a user with the session id """
+        session_id = self.session_cookie(request)
+        return User.get(self.user_id_for_session_id(session_id))

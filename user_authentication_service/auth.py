734ff03fff283f3a30f72c240d553999b992ee25
Cyprien <cypriengehu@example.com>
2025-11-17 12:37:09 +0100

    feat: task 5, register a user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 """authentification system"""
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database."""
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """register a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return User
+        raise ValueError("User {} already exists".format(email))

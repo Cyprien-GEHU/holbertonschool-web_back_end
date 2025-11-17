f76091eda960863e2f806c6ce831bcadda214db5
Cyprien <cypriengehu@example.com>
2025-11-17 11:48:48 +0100

    feat: task2, find a user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -4,6 +4,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 
 from user import Base, User
 
@@ -29,7 +32,18 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user on the database"""
         users = User(email=email, hashed_password=hashed_password)
         self._session.add(users)
         self._session.commit()
         return users
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a User"""
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        except TypeError:
+            raise InvalidRequestError
+        if user is None:
+            raise NoResultFound
+        return user

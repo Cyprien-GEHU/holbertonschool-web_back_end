fb4417998575f398cb063968d54b0ab15f57ab45
Cyprien <cypriengehu@example.com>
2025-11-18 15:16:59 +0100

    task 17, get reset password

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -76,3 +76,12 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            new_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=new_password,
+                                 reset_token=None)
+        except Exception:
+            raise ValueError

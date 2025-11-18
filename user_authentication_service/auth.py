1c0034f486ff26fb745115400f025b96c839a3ea
Cyprien <cypriengehu@example.com>
2025-11-18 10:46:06 +0100

    fix: task 13 correction of user_id type

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -61,6 +61,6 @@ class Auth:
         else:
             return None
 
-    def destroy_session(self, user_id: str) -> None:
+    def destroy_session(self, user_id: int) -> None:
         """destroy the session"""
         self._db.update_user(user_id, session_id=None)

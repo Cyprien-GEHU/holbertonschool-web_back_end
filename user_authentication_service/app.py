ad5bbca62d3f668d7a96e0e4251b7f1cde68a0c3
Cyprien <cypriengehu@example.com>
2025-11-18 11:19:31 +0100

    task 14, log out

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """app system"""
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 
@@ -37,6 +37,17 @@ def login():
         response = jsonify({"email": email, "message": "logged in"})
         response.set_cookie('session_id', session_id)
         return response
+    else:
+        abort(401)
+
+
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout():
+    session_cookie = request.cookie.get("session_id")
+    user = AUTH.get_user_from_session_id(session_cookie)
+    if user:
+        AUTH.destroy_session(user.id)
+        return redirect('/')
     else:
         abort(401)
 

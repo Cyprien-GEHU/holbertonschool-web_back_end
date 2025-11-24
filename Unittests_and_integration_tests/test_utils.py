ccbc1767bcb5fe98dc2b7e2909dd4ad833098f60
Cyprien <cypriengehu@example.com>
2025-11-24 11:50:06 +0100

    feat: task 0, Parameterize a unit test

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""unit test program"""
+
+import unittest
+from parameterized import parameterized
+from unittest.mock import Mock
+from utils import access_nested_map, memoize, get_json
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Nested mapd test """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map, path, result):
+        """ test acces nested map function"""
+        self.assertEqual((nested_map, path), result)

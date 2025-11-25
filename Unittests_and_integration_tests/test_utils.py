06d6d91f2b9ab0e2bc43e0d54dd6de7c6a44f671
Cyprien <cypriengehu@example.com>
2025-11-25 11:03:19 +0100

    feat: task 3, Parameterize and patch

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -45,3 +45,23 @@ class TestGetJson(unittest.TestCase):
 
             mock_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestCase(unittest.TestCase):
+    """ Test Case class"""
+    def test_memoize(self):
+        """test memoize method"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        with patch.object(TestClass, "a_method") as mock_method:
+            call = TestClass()
+            call.a_property
+            call.a_property
+            mock_method.assert_called_once()

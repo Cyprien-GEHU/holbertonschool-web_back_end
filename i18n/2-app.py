3a2b17357aba6100d1fbf2dc4e26a594c4e8b20f
Cyprien <cypriengehu@example.com>
2025-11-26 14:29:18 +0100

    fix :task 2, correct of the locale_selector part

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -18,10 +18,11 @@ class Config():
 app.config.from_object(Config)
 
 
-@babel.localeselector
 def get_local():
-    """ get local with babel"""
+    """ get local with babel """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_local)
 
 
 @app.route('/', methods=['GET'])

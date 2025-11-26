2f31983bb2d4337b853558d87ed27cd2a1e82246
Cyprien <cypriengehu@example.com>
2025-11-26 14:10:57 +0100

    feat: task 1,  Basic Babel setup

---
+++ i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""flask aplication"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.__class__(Config)
+
+
+@app.route('/', methods=['GET'])
+def htmlTemplate():
+    """ Using html templates """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()

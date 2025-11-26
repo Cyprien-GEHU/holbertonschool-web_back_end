8ff5863b6e7cdf0d1bdbd7c1d6a7a52c65c5f19b
Cyprien <cypriengehu@example.com>
2025-11-26 11:33:50 +0100

    fix: task 0, pycodestyle

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=['GET'])
 def htmlTemplate():
     """ Using html templates """
     return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run()

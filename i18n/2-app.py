03381369b4a6fe78cc6677fba35ddc5002be0fb1
Cyprien <cypriengehu@example.com>
2025-11-26 14:22:54 +0100

    feat: task 2, Get locale from request

---
+++ i18n/2-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""flask aplication"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config():
+    """ config babel """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_local():
+    """ get local with babel"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'])
+def htmlTemplate():
+    """ Using html templates """
+    return render_template("2-index.html")
+
+
+if __name__ == "__main__":
+    app.run()

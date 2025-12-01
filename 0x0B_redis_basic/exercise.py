09b5721e3f2d93203a62562647cfa81ddc09fc29
Cyprien <cypriengehu@example.com>
2025-12-01 09:45:54 +0100

    feat: task 0, Writing strings to Redis and Readme

---
+++ 0x0B_redis_basic/exercise.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Redis exercise file
+"""
+import redis
+import uuid
+from typing import Union
+
+
+redis_data = Union[int, str, bytes, float]
+
+
+class Cache():
+    """class cache for the memory"""
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: redis_data) -> str:
+        """ the method generate a random key """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key

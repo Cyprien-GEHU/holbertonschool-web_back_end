3de9ff2a629e5c3d0f0d4f05088268b22eed4feb
Cyprien <cypriengehu@example.com>
2025-12-01 09:56:15 +0100

    fix: task 0, change the union declaration

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,9 +7,6 @@ import uuid
 from typing import Union
 
 
-redis_data = Union[int, str, bytes, float]
-
-
 class Cache():
     """class cache for the memory"""
     def __init__(self):
@@ -17,7 +14,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: redis_data) -> str:
+    def store(self, data: Union[int, str, bytes, float]) -> str:
         """ the method generate a random key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)

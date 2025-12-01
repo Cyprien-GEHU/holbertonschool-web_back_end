a42a6190ce5be5956d2be7a246d0d3a1bece9bdb
Cyprien <cypriengehu@example.com>
2025-12-01 10:06:44 +0100

    fix : task 0, change the order on Union

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -7,6 +7,9 @@ import uuid
 from typing import Union
 
 
+redis_data = Union[str, bytes, int, float]
+
+
 class Cache():
     """class cache for the memory"""
     def __init__(self):
@@ -14,7 +17,7 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: Union[int, str, bytes, float]) -> str:
+    def store(self, data: redis_data) -> str:
         """ the method generate a random key """
         key = str(uuid.uuid4())
         self._redis.set(key, data)

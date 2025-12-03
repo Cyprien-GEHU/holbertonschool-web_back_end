69fd834ef4eeb1acbe5828077e9456036d3a0f31
Cyprien <cypriengehu@example.com>
2025-12-03 11:12:59 +0100

    feat: task 5, Email validation to sent

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,12 @@
+-- create a trigger that reset the attribue valid_email only when email has been changed
+DELIMITER $$
+CREATE TRIGGER new_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF NEW.email != OLD.email
+        THEN SET NEW.valid_email = 0;
+    END IF;
+END $$
+
+DELIMITER ;

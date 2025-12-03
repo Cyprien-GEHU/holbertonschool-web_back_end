0f6465a84e0c0f6caf3559d28b3816371eaeddcd
Cyprien <cypriengehu@example.com>
2025-12-03 10:19:32 +0100

    fix : task 2 correction name nb_fans

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranks country origin of bande, order by the number of fans
 
-SELECT origin, SUM(fans) AS nb_fan
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fan DESC;
+ORDER BY nb_fans DESC;

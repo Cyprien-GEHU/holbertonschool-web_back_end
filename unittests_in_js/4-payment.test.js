df401f8e55f4ec0129e803fd62c892575da75be8
Cyprien <cypriengehu@example.com>
2025-12-10 11:06:28 +0100

    fix task4

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -4,11 +4,11 @@ const assert = require("assert");
 const Utils = require("./utils");
 
 describe("Send request to api", () => {
-    it("return the sum of total amount and total shipping", () => {
+    it("return 10 with the console.log and sinon.stub", () => {
         const stub = sinon.stub(Utils, "calculateNumber").returns(10);
         const spy = sinon.spy(console, "log");
 
-        sendPaymentRequestToApi(100,20);
+        sendPaymentRequestToApi(100, 20);
         assert(stub.calledOnceWithExactly("SUM", 100, 20));
         assert(spy.calledOnceWithExactly("The total is: 10"));
 

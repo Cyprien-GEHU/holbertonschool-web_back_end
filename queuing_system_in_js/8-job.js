6dfe1ab87b683392c83103c95143a79609fa31b3
Cyprien <cypriengehu@example.com>
2025-12-18 11:11:08 +0100

    Feat : task 10, Writing the job creation function

---
+++ queuing_system_in_js/8-job.js
@@ -0,0 +1,29 @@
+export default function createPushNotificationsJobs(jobs, queue) {
+    if(!Array.isArray(jobs))
+    {
+        throw new Error("Jobs is not an array")
+    }
+
+    jobs.forEach((job) => {
+        const one_job = queue.create("push_notification_code_2", job).save((err) => {
+        if(!err) {
+            console.log(`Notification job created: ${one_job.id}`)
+        } else {
+            console.log("ERROR:", err)
+        }
+    })
+
+
+        one_job.on("completed", () => {
+            console.log(`Notification job ${one_job.id} completed`);
+        });
+
+        one_job.on("failed", (error) => {
+            console.log(`Notification job ${one_job.id} failed: ${error}`);
+        });
+
+        one_job.on("progress", (progress) => {
+            console.log(`Notification job ${one_job.id} ${progress}% complete`);
+        });
+    })
+}
